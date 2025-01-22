8d70bdd5876de67286c04b4947cfc4cff160bc7b
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-01-23 00:43:39 +0100

    Try some chrom Flags

--- run-tests.js
+++ run-tests.js
@@ -108,7 +108,7 @@ async function tryToPreventNetlifyBuildTimeout(dateTestsStarted, numberOfUrls, e
         let runCount =
             group.options && group.options.runs ? group.options.runs : NUMBER_OF_RUNS;
         let options = Object.assign({
-            chromeFlags: ['--headless', '--disable-dev-shm-usage']
+            chromeFlags: ['--headless', '--disable-dev-shm-usage',"--disable-gpu", "--no-sandbox", "--no-zygote"]
         }, group.options);
 
         let results = await PerfLeaderboard(
